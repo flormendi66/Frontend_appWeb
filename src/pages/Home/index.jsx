f0cb9013c7ec8d464638ba6b68aaf2b84adbf503
fmarcos38 <fmarcos_23@hotmail.com>
2024-09-20 15:22:18 -0300

    paginacion home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,56 +12,69 @@ import './styles.css';
 function Home() {
     const loading = useSelector(state => state.loading);
     const allProps = useSelector(state => state.propiedades);
-    const totalPropiedades = useSelector(state => state.totPropiedades); // el total viene del backend
+    const totalPropiedades = useSelector(state => state.totPropiedades);
     const dispatch = useDispatch();
 
-    const [currentPage, setCurrentPage] = useState(1);  // Estado para la página actual
-    const propiedadesPorPagina = 20;  // Definimos el límite de propiedades por página
+    const [currentPage, setCurrentPage] = useState(1);
+    const propiedadesPorPagina = 12; // Definir el límite de propiedades por página de forma constante
+    const limit = propiedadesPorPagina;
 
-    
+    const [operacion, setOperacion] = useState('');  // Filtro de operación
+    const [tipoPropiedad, setTipoPropiedad] = useState('todas');  // Filtro de tipo de propiedad
+
+    // Calcula el offset basado en la página actual
+    const offset = (currentPage - 1) * limit;
+
+    // Efecto para manejar la paginación y los filtros
     useEffect(() => {
-        const offset = (currentPage - 1) * propiedadesPorPagina;
-        dispatch(getProps(propiedadesPorPagina, offset));
-    }, [dispatch, currentPage]);
+        dispatch(getProps(limit, offset, operacion, tipoPropiedad));
+    }, [dispatch, currentPage, limit, offset, operacion, tipoPropiedad]);
 
     return (
         <div>
             {
-                loading ?
-                (
-                    <Loading/>
-                    ) : (
-                        <div className='cont-home'>
-                            {/* landing */}
-                            <LandingPage />
-                            
-                            {/* contenedor filtros y lista props */}
-                            <div className='cont-filtros-listaProps'>
-                                {/* filtros */}
-                                <div className='cont-barraL'>                                    
-                                    <BarraLateral muestraVentaAlq={'true'}/>
-                                </div>                               
+                loading ? (
+                    <Loading />
+                ) : (
+                    <div className='cont-home'>
+                        {/* landing */}
+                        <LandingPage />
 
-                                {/* lista props */}
-                                <div className='cont-listaProps'>
-                                    <ListaPropiedades allProps={allProps} id='listaProps'/>
+                        {/* contenedor filtros y lista props */}
+                        <div className='cont-filtros-listaProps'>
+                            {/* filtros */}
+                            <div className='cont-barraL'>
+                                <BarraLateral
+                                    muestraVentaAlq={'true'}
+                                    limit={limit}  // Aquí pasamos el valor de limit al componente BarraLateral
+                                    offset={offset} // También pasamos el offset
+                                    setCurrentPage={setCurrentPage}
+                                    setOperacion={setOperacion}
+                                    setTipoPropiedad={setTipoPropiedad}  // Nuevo prop para manejar tipoPropiedad
+                                />
+                            </div>
 
-                                    <Paginacion 
-                                        allProps={allProps}
-                                        currentPage={currentPage} 
-                                        onPageChange={setCurrentPage} 
-                                        totalPropiedades={totalPropiedades}
-                                    />
-                                </div>
-                            </div> 
+                            {/* lista props */}
+                            <div className='cont-listaProps'>
+                                <ListaPropiedades allProps={allProps} id='listaProps' />
 
-                            {/* botón whatsApp */}
-                            <WhatsAppButton/>
+                                <Paginacion
+                                    allProps={allProps}
+                                    currentPage={currentPage}
+                                    onPageChange={setCurrentPage}
+                                    totalPropiedades={totalPropiedades}
+                                    propiedadesPorPagina={propiedadesPorPagina}
+                                />
+                            </div>
                         </div>
-                    )
+
+                        {/* botón WhatsApp */}
+                        <WhatsAppButton />
+                    </div>
+                )
             }
         </div>
-    )
+    );
 }
 
 export default Home;
--- src/pages/PropsVenta/index.jsx
+++ src/pages/PropsVenta/index.jsx
@@ -1,64 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProps } from '../../Redux/Actions';
-import ListaPropiedades from '../../components/ListaPropiedades';
-import BarraLateral from '../../components/Barra-Lateral';
-import Loading from '../../components/Loading';
-import Paginacion from '../../components/Paginacion';
-import WhatsAppButton from '../../components/BotonWhastApp';
+import React from 'react';
 import './estilos.css';
 
 
 function PropsVenta() {
 
-    const loading = useSelector(state => state.loading);
-    const props = useSelector(state => state.propiedades);   
-    const [currentPage, setCurrentPage] = useState(1);  // Estado para la página actual
-    const propiedadesPorPagina = 20;  // Definimos el límite de propiedades por página
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        const offset = (currentPage - 1) * propiedadesPorPagina;
-        dispatch(getProps(propiedadesPorPagina, offset));
-    }, [dispatch, currentPage]);
 
-    //filtro solo venta
-    const soloEnVenta = props.filter(p => p.operacion[0].operacion === 'Venta');
 
     return (
-        <div className='cont-prop-Venta'>
-            <h1>Propiedade en venta</h1>
-            {
-                loading ? (
-                    <Loading/>
-                ) : (
-                    <>
-                        {/* contenedor filtros y lista props */}
-                        <div className='cont-filtros-listaProps'>
-                                {/* filtros */}
-                                <div className='cont-barraL'>                                    
-                                    <BarraLateral muestraVentaAlq={false}/>
-                                </div>                               
-
-                                {/* lista props */}
-                                <div className='cont-listaProps'>
-                                    <ListaPropiedades allProps={soloEnVenta} id='listaProps'/>
-
-                                    <Paginacion 
-                                        allProps={soloEnVenta}
-                                        currentPage={currentPage} 
-                                        onPageChange={setCurrentPage} 
-                                        totalPropiedades={soloEnVenta.length}
-                                    />
-                                </div>
-                            </div> 
-
-                            {/* botón whatsApp */}
-                            <WhatsAppButton/>
-                    </>
-                )
-            }
-        </div>
+        <div></div>
     )
 }
 
