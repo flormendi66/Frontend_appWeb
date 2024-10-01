8dcb038d87a62516b5958fa8ab91c5fab324f365
fmarcos38 <fmarcos_23@hotmail.com>
2024-10-01 10:51:00 -0300

    filtra por precios

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,6 +1,6 @@
 //desarrollo
-const desarrollo = "http://localhost:3001";
+//const desarrollo = "http://localhost:3001";
 //producción
 const produccion = "https://flowing-bonito-434714-t7.rj.r.appspot.com";
 
-export const actual = desarrollo;
+export const actual = produccion;
