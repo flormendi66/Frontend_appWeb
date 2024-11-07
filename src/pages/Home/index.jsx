fab2a446608d26629572255afc7f00324a08f8d7
fmarcos38 <fmarcos_23@hotmail.com>
2024-11-07 10:09:05 -0300

    filtros alq temp

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,16 +10,12 @@ import BarraLateral from '../../components/Barra-Lateral';
 import WhatsAppButton from '../../components/BotonWhastApp';
 import Paginacion from '../../components/Paginacion';
 import logoTexto from '../../Imagenes/logoTexto.png';
-import imgHome1 from '../../Imagenes/imgHome1.jpg';
-import imgHome2 from '../../Imagenes/imgHome2.jpg';
 import './styles.css';
 
 
 function Home() {
     
     const loading = useSelector(state => state.loading);
-    const [isFixed, setIsFixed] = useState(false);
-    const [isVisible, setIsVisible] = useState(true); // Para controlar la visibilidad
     const [operacion, setOperacion] = useState('');
     const [tipoPropiedad, setTipoPropiedad] = useState('todas');
     const [currentPage, setCurrentPage] = useState(1);
@@ -42,32 +38,6 @@ function Home() {
         dispatch(getProps(limit, offset, operacion, tipoPropiedad));
     }, [dispatch, currentPage, limit, offset, operacion, tipoPropiedad]);
 
-    // Efecto para fijar el div "textos"
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            const imagenesFondo = document.getElementById('imagenes-fondo');
-            const imagenesHeight = imagenesFondo ? imagenesFondo.offsetHeight : 0;
-
-            if (scrollY >= 700 && scrollY < 3000 + imagenesHeight) {
-                setIsFixed(true);
-            } else {
-                setIsFixed(false);
-            }
-
-            if (scrollY >= 1700 + imagenesHeight) {
-                setIsVisible(false);
-            } else {
-                setIsVisible(true);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
 
     return (
         <div>
@@ -79,38 +49,20 @@ function Home() {
                         {/* landing */}
                         <Landing2 />
 
-                        <div className='cont-texto-imagenes'>
-                                {isVisible && (
-                                    <div className={`textos-home ${isFixed ? 'fixed' : ''}`} id="textos">
-                                        <div className='subCont-textos'>
-                                            <p className="texto-somos">SOMOS</p>
-                                            <img src={logoTexto} alt='' className='logoTexto' />
-                                            <p className="texto-sup">
-                                                Nos dedicamos a realizar gestiones inmobiliarias, con un enfoque en propiedades de diseño único.
-                                                <br />
-                                                Creemos en la importancia de caminar juntos hacia el cumplimiento de tus objetivos, para que sientas nuestro respaldo en cada paso del proceso.
-                                            </p>
-                                            <Link to={'/contacto'}>
-                                                <button className='btn-contactanos-home'>Contactanos</button>
-                                            </Link>                                            
-                                        </div>
-                                    </div>
-                                )}
-                                <div className='cont-imgenes-fondo' id="imagenes-fondo">
-                                    <div className='cont-sup-img'>
-                                        <div className='cont-fondo-img'>
-                                            <img src={imgHome1} alt='' className='img-fondo' />
-                                        </div>
-                                        <div className='cont-fondo-oscuro'></div>
-                                    </div>
-                                    <div className='cont-inf-img'>
-                                        <div className='cont-fondo-oscuro'></div>
-                                        <div className='cont-fondo-img'>
-                                            <img src={imgHome2} alt='' className='img-fondo' />
-                                        </div>
-                                    </div>
+                            <div className='cont-texto-imagenes'>
+                                <div className='subCont-textos'>
+                                    <p className="texto-somos">SOMOS</p>
+                                    <img src={logoTexto} alt='' className='logoTexto' />
+                                    <p className="texto-sup">
+                                        Nos dedicamos a realizar gestiones inmobiliarias, con un enfoque en propiedades de diseño único.
+                                        <br />
+                                        Creemos en la importancia de caminar juntos hacia el cumplimiento de tus objetivos, para que sientas nuestro respaldo en cada paso del proceso.
+                                    </p>
+                                    <Link to={'/contacto'}>
+                                        <button className='btn-contactanos-home'>Contactanos</button>
+                                    </Link>
                                 </div>
-                        </div>
+                            </div>
 
                         {/* contenedor filtros y lista props */}
                         <div className='cont-titulo-filtros-props'>
