3aff6dc979cb32ae1fcbf331d5965202c8ba573a
fmarcos38 <fmarcos_23@hotmail.com>
2024-12-12 19:21:43 -0300

    video

--- src/urls/index.js
+++ src/urls/index.js
@@ -3,4 +3,4 @@ const desarrollo = "http://localhost:3001";
 //producción
 const produccion = "https://inmobiliariaflor-b5b5afc17722.herokuapp.com";
 
-export const actual = desarrollo;
+export const actual = produccion;
