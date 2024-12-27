81f348c5715a5133cdcdb3574cf4ed07d5d3173b
fmarcos38 <fmarcos_23@hotmail.com>
2024-12-27 15:18:18 -0300

    modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
-import { isOpenModalPicture } from '../../Redux/Actions';
 import CarruselModal from '../CarruselModal';
 import './styles.css';
 
 function Modal({imagenes}) {
 
-    const dispatch = useDispatch();
-
-    const handleOnclickClose = () =>{
-        dispatch(isOpenModalPicture());
-    };
+    
 
     return (
         <div className='contModal'>
             <p className='mesaj-pos-horizontal'>Gira el telefono a posición horizontal</p>
-            <div className='cont-btn-cierra-modal'>
-                <button
-                    className='btn-close-modal'
-                    onClick={() => handleOnclickClose()}
-                >
-                    <b>X</b>
-                </button>
-            </div>
 
             {/* carrusel de imgs */}
             <div className='cont-carrusel-modal'>
